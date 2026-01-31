1e14e4ac68f927aa2c0e80ccbe0449d3cafa593c
Berangere Br <brunberangere@gmail.com>
2026-01-31 18:18:09 +0100

    création du snake + déplacement du snake

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,7 +1,7 @@
 const GridSize = 20;
 const CellSize = 30;
 
-function Board() {
+function Board({ snake }) {
     return (
         <div
             style={{
@@ -10,17 +10,25 @@ function Board() {
                 gridTemplateRows: `repeat(${GridSize},${CellSize}px)`,
                 border: '2px solid black',
             }}>
-            {Array.from({ length: GridSize * GridSize }).map((_, index) => (
-                <div
-                    key={index}
-                    style={{
-                        width: CellSize,
-                        height: CellSize,
-                        border: '2px solid black',
-                    }}>
-                </div>
-            ))}
-        </div>
+            {Array.from({ length: GridSize * GridSize }).map((_, index) => {
+                const x = index % GridSize
+                const y = Math.floor(index / GridSize)
+
+                let isSnake = snake.some(
+                    (segment) => segment.x === x && segment.y === y
+                )
+                return (
+                    < div
+                        key={index}
+                        style={{
+                            width: CellSize,
+                            height: CellSize,
+                            border: '2px solid black',
+                            backgroundColor: isSnake ? 'green' : 'white',
+                        }}>
+                    </div>)
+            })}
+        </div >
     )
 }
 
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,9 +1,61 @@
 import Board from "./Board"
+import { useState, useEffect } from "react";
 
 function Game() {
+    const [snake, setSnake] = useState([{ x: 10, y: 10 }]);
+    const [direction, setDirection] = useState('RIGHT');
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setSnake((prevSnake) => {
+                const head = prevSnake[0];
+                let newHead
+                switch (direction) {
+                    case "RIGHT":
+                        newHead = { x: head.x + 1, y: head.y }
+                        break
+                    case "LEFT":
+                        newHead = { x: head.x - 1, y: head.y }
+                        break
+                    case "UP":
+                        newHead = { x: head.x, y: head.y - 1 }
+                        break
+                    case "DOWN":
+                        newHead = { x: head.x, y: head.y + 1 }
+                        break
+                }
+                const newSnake = [newHead, ...prevSnake];
+                newSnake.pop()
+                setSnake(newSnake)
+            })
+        }, 200)
+        return () => clearInterval(interval)
+    }, [direction])
+
+    useEffect(() => {
+        function handleKey(e) {
+            switch (e.key) {
+                case "ArrowUp":
+                    if (direction !== "DOWN") setDirection("UP")
+                    break
+                case "ArrowDown":
+                    if (direction !== "UP") setDirection("DOWN")
+                    break
+                case "ArrowLeft":
+                    if (direction !== "RIGHT") setDirection("LEFT")
+                    break
+                case "ArrowRight":
+                    if (direction !== "LEFT") setDirection("RIGHT")
+                    break
+            }
+        }
+        window.addEventListener("keydown", handleKey)
+        return () => window.removeEventListener("keydown", handleKey)
+    }, [direction])
+
     return (
-        <Board />
+        <Board snake={snake} />
     )
 }
 
-export default Game
+export default Game;
