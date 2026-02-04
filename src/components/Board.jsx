a89f94f8e1d3d1c7706043815d0e30991bc08976
Berangere Br <brunberangere@gmail.com>
2026-02-04 13:24:15 +0100

    ajout du score

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,34 +2,38 @@ const GRID_SIZE = 20;
 const CELL_SIZE = 30;
 
 function Board({ snake, food }) {
+    const score = snake.length - 1;
     return (
-        <div
-            style={{
-                display: 'grid',
-                gridTemplateColumns: `repeat(${GRID_SIZE},${CELL_SIZE}px)`,
-                gridTemplateRows: `repeat(${GRID_SIZE},${CELL_SIZE}px)`,
-                border: '2px solid black',
-            }}>
-            {Array.from({ length: GRID_SIZE * GRID_SIZE }).map((_, index) => {
-                const x = index % GRID_SIZE
-                const y = Math.floor(index / GRID_SIZE)
+        <div>
+            <div
+                style={{
+                    display: 'grid',
+                    gridTemplateColumns: `repeat(${GRID_SIZE},${CELL_SIZE}px)`,
+                    gridTemplateRows: `repeat(${GRID_SIZE},${CELL_SIZE}px)`,
+                    border: '2px solid black',
+                }}>
+                {Array.from({ length: GRID_SIZE * GRID_SIZE }).map((_, index) => {
+                    const x = index % GRID_SIZE
+                    const y = Math.floor(index / GRID_SIZE)
 
-                let isSnake = snake.some(
-                    (segment) => segment.x === x && segment.y === y
-                )
-                const isFood = food.x === x && food.y === y
-                return (
-                    <div
-                        key={index}
-                        style={{
-                            width: CELL_SIZE,
-                            height: CELL_SIZE,
-                            border: '1px solid black',
-                            backgroundColor: isSnake ? 'green' : isFood ? 'red' : 'white',
-                        }}>
-                    </div>)
-            })}
-        </div >
+                    let isSnake = snake.some(
+                        (segment) => segment.x === x && segment.y === y
+                    )
+                    const isFood = food.x === x && food.y === y
+                    return (
+                        <div
+                            key={index}
+                            style={{
+                                width: CELL_SIZE,
+                                height: CELL_SIZE,
+                                border: '1px solid black',
+                                backgroundColor: isSnake ? 'green' : isFood ? 'red' : 'white',
+                            }}>
+                        </div>)
+                })}
+            </div >
+            <div>Score : {score}</div>
+        </div>
     )
 }
 
