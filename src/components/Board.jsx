77e30b7baea0d7a03af16941f219aa711e5b8c87
Berangere Br <brunberangere@gmail.com>
2026-02-16 22:02:45 +0100

    ajout des fleche pour les boutons mobile/redimention grilla snake pour mobile

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,19 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import arrow from "../assets/images/arrow.png";
 
 const GRID_SIZE = 15;
 const CELL_SIZE = 30;
+const CELL_SIZE_MOBILE = 20;
+const SCREEN_SIZE = 1024;
 
 function Board({ snake, food, score, playing, onStart, countdown, openModalGameover, replay, onDirection }) {
     const [openModalSnakeGuide, setOpenModalSnakeGuide] = useState(true);
+    const [isMobile, setIsMobile] = useState(window.innerWidth < SCREEN_SIZE);
+
+    useEffect(() => {
+        function handleResize() {
+            setIsMobile(window.innerWidth < SCREEN_SIZE);
+        }
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    })
 
     return (
         <div className="relative flex flex-col justify-center items-center w-screen h-screen gap-10">
             <div
                 className='grid rounded shadow-[0px_0px_40px_2px_#FF00FF] '
-                style={{
-                    gridTemplateColumns: `repeat(${GRID_SIZE},${CELL_SIZE}px)`,
-                    gridTemplateRows: `repeat(${GRID_SIZE},${CELL_SIZE}px)`,
-                }}>
+                style={isMobile ?
+                    {
+                        gridTemplateColumns: `repeat(${GRID_SIZE},${CELL_SIZE_MOBILE}px)`,
+                        gridTemplateRows: `repeat(${GRID_SIZE},${CELL_SIZE_MOBILE}px)`,
+                    } :
+                    {
+                        gridTemplateColumns: `repeat(${GRID_SIZE},${CELL_SIZE}px)`,
+                        gridTemplateRows: `repeat(${GRID_SIZE},${CELL_SIZE}px)`,
+                    }}>
                 {Array.from({ length: GRID_SIZE * GRID_SIZE }).map((_, index) => {
                     const x = index % GRID_SIZE
                     const y = Math.floor(index / GRID_SIZE)
@@ -27,39 +44,60 @@ function Board({ snake, food, score, playing, onStart, countdown, openModalGameo
                     return (
                         <div
                             key={index}
-                            style={{
-                                width: CELL_SIZE,
-                                height: CELL_SIZE,
-                                border: '1px solid #DBDBDB',
-                                backgroundColor: headSnake ? '#27F52E' : isSnake ? '#79FF3C' : isFood ? '#FF00FF' : 'white',
-                            }}>
+                            style={isMobile ?
+                                {
+                                    width: CELL_SIZE_MOBILE,
+                                    height: CELL_SIZE_MOBILE,
+                                    border: '1px solid #DBDBDB',
+                                    backgroundColor: headSnake ? '#27F52E' : isSnake ? '#79FF3C' : isFood ? '#FF00FF' : 'white',
+                                } :
+                                {
+                                    width: CELL_SIZE,
+                                    height: CELL_SIZE,
+                                    border: '1px solid #DBDBDB',
+                                    backgroundColor: headSnake ? '#27F52E' : isSnake ? '#79FF3C' : isFood ? '#FF00FF' : 'white',
+                                }}>
                         </div>)
                 })}
                 {!playing && countdown === null && openModalGameover === false &&
-                    <div style={{
-                        width: GRID_SIZE * CELL_SIZE,
-                        height: GRID_SIZE * CELL_SIZE,
-                    }}
+                    <div style={isMobile ?
+                        {
+                            width: GRID_SIZE * CELL_SIZE_MOBILE,
+                            height: GRID_SIZE * CELL_SIZE_MOBILE,
+                        } : {
+                            width: GRID_SIZE * CELL_SIZE,
+                            height: GRID_SIZE * CELL_SIZE,
+                        }}
                         className="absolute flex justify-center items-center bg-[#FF00FF]/30">
                         <button onClick={onStart}
                             className="rounded cursor-pointer pt-2 pb-2 pl-8 pr-8  bg-black font-retro text-5xl text-[#27F52E] hover:scale-110 hover:shadow-[0_0_15px_#27F52E]">Jouer
                         </button>
                     </div>}
                 {countdown !== null &&
-                    <div style={{
+                    <div style={isMobile ?
+                        {
+                            width: GRID_SIZE * CELL_SIZE_MOBILE,
+                            height: GRID_SIZE * CELL_SIZE_MOBILE,
+                        } :
+                        {
+                            width: GRID_SIZE * CELL_SIZE,
+                            height: GRID_SIZE * CELL_SIZE,
+                        }}
+                        className='absolute flex justify-center items-center text-[#27F52E] text-[200px] font-retro '>{countdown === 0 ? 'GO!' : countdown}</div>}
+                {openModalGameover ? <div style={isMobile ?
+                    {
+                        width: GRID_SIZE * CELL_SIZE_MOBILE,
+                        height: GRID_SIZE * CELL_SIZE_MOBILE,
+                    } :
+                    {
                         width: GRID_SIZE * CELL_SIZE,
                         height: GRID_SIZE * CELL_SIZE,
                     }}
-                        className='absolute flex justify-center items-center text-[#27F52E] text-[200px] font-retro '>{countdown === 0 ? 'GO!' : countdown}</div>}
-                {openModalGameover ? <div style={{
-                    width: GRID_SIZE * CELL_SIZE,
-                    height: GRID_SIZE * CELL_SIZE,
-                }}
                     className="absolute flex justify-center items-center bg-black bg-opacity-50">
                     <div className="flex flex-col justify-center items-center bg-black max-h-[40vh] w-[80%] rounded gap-5 animate-gameover-spin p-5">
                         <p className="text-[#FF00FF] font-retro text-6xl text-center">GAME OVER</p>
                         <p className="text-[#FF00FF] font-sans">Score : {score}</p>
-                        <button onClick={replay} className="rounded cursor-pointer  p-2 bg-[#27F52E] font-retro text-black text-4xl hover:scale-110 hover:shadow-[0_0_15px_#27F52E] w-[120px] md:w-[150px]">Rejouer</button>
+                        <button onClick={replay} className="rounded cursor-pointer p-2 bg-[#27F52E] font-retro text-black text-4xl hover:scale-110 hover:shadow-[0_0_15px_#27F52E] w-[120px] md:w-[150px]">Rejouer</button>
                     </div>
                 </div> : null}
             </div >
@@ -71,12 +109,12 @@ function Board({ snake, food, score, playing, onStart, countdown, openModalGameo
                     <p className="font-sans text-white text-base md:text-xl text-justify">Utilisez les flèches du clavier ou ZQSD pour diriger le serpent et manger la nourriture. Chaque fois que vous mangez, le serpent grandit. Faites attention à ne pas vous mordre la queue ou à sortir des limites du plateau de jeu !</p>
                 </div>
             </div>}
-            <div className="grid grid-cols-3 gap-2">
-                <button className=" bg-white col-start-2 cursor-pointer" onClick={() => onDirection("UP")}>up</button>
-                <button className=" bg-white col-start-1 cursor-pointer" onClick={() => onDirection("LEFT")}>left</button>
-                <button className=" bg-white cursor-pointer" onClick={() => onDirection("DOWN")}>down</button>
-                <button className=" bg-white cursor-pointer" onClick={() => onDirection("RIGHT")}>right</button>
-            </div>
+            {isMobile && <div className="grid grid-cols-3 gap-2">
+                <button className=" bg-[#27F52E] col-start-2 cursor-pointer rounded p-2 hover:scale-110" onClick={() => onDirection("UP")}><img src={arrow} alt="flèche directionnelle" className="w-10 h-10 rotate-180" /></button>
+                <button className=" bg-[#27F52E] col-start-1 cursor-pointer rounded p-2 hover:scale-110" onClick={() => onDirection("LEFT")}><img src={arrow} alt="flèche directionnelle" className="w-10 h-10 rotate-90" /></button>
+                <button className=" bg-[#27F52E] cursor-pointer rounded p-2 hover:scale-110" onClick={() => onDirection("DOWN")}><img src={arrow} alt="flèche directionnelle" className="w-10 h-10" /></button>
+                <button className=" bg-[#27F52E] cursor-pointer rounded p-2 hover:scale-110" onClick={() => onDirection("RIGHT")}><img src={arrow} alt="flèche directionnelle" className="w-10 h-10 -rotate-90" /></button>
+            </div>}
         </div >
     )
 }
